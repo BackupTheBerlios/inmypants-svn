6635255734a1f911445764e829b221c65de66ae0
dave2 <dave2@37a86406-e6d5-0310-9a85-ba2c43b180f2>
2004-05-26 08:22:26 +0000

    At the request of trust`, you can now say "piss in foo".

--- plugins/en/complex_piss.tcl
+++ plugins/en/complex_piss.tcl
@@ -9,11 +9,11 @@
 # in the modules directory.
 ###############################################################################
 
-IMP_plugin_add_complex "piss" "^%botnicks:?,? (wee|piss|pee|urinate|empty your bladder) on " 100 IMP_plugin_complex_piss "en"
+IMP_plugin_add_complex "piss" "^%botnicks:?,? (wee|piss|pee|urinate|empty your bladder) (on|in) " 100 IMP_plugin_complex_piss "en"
   
 proc IMP_plugin_complex_piss { nick host handle channel text } {
 	global botnicks
-	if [regexp -nocase "${botnicks}:?,? (please)?(piss|pee|urinate|empty your bladder) on (.+?)" $text ming ming1 ming2 ming3 who] {
+	if [regexp -nocase "${botnicks}:?,? (please)?(piss|pee|urinate|empty your bladder) (on|in) (.+?)" $text ming ming1 ming2 ming3 ming4 who] {
 	
 		if [regexp -nocase "(himself|herself|itself|themsel)" $who] {
 			set who $nick
