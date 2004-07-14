76e897ac9edd182cd7eadb48a482b4952716f4e2
dave2 <dave2@37a86406-e6d5-0310-9a85-ba2c43b180f2>
2004-07-14 18:24:54 +0000

    Added simple_luigi30.tcl. Apparently.

---
+++ plugins/en/simple_luigi30.tcl
@@ -0,0 +1,12 @@
+## IMP simple plugin: sing
+
+###############################################################################
+# This is a IMP plugin
+# Copyright (C) Dave Wickham 2004
+#
+# This program is covered by the GPL, please refer the to LICENCE file in the
+# distribution; further information can be found in the headers of the scripts
+# in the modules directory.
+###############################################################################
+
+IMP_plugin_add_simple "luigi30" "%botnicks,?:? Luigi30" 100 "%VAR{fears}" "en"
